166f7d7455c8957c16f69776ba7d879357790b8b
Micah Koki <micah.koki@gmail.com>
2016-01-21 09:46:07 -1000

    denormal cars sql section done

---
+++ denormal_cars.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT make_title FROM car_models;
+
+SELECT DISTINCT model_title FROM car_models WHERE make_code = 'VOLKS';
+
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+
+SELECT * FROM car_models WHERE year BETWEEN 2010 and 2015;
